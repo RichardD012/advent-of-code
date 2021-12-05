053bd7802a44c97300d654492aa27d2309a5c61d
Richard Dyer <Richard@r7development.com>
2021-12-04 20:00:23 -0700

    Adding day 6 placeholders

---
+++ src/Tasks/2020/Day6Task.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using AdventCode.Utils;
+using Microsoft.Extensions.Logging;
+using AdventCode.Tasks;
+
+namespace AdventCode.Tasks2020;
+
+public class Day6Task : BaseCodeTask, IAdventCodeTask
+{
+    public override int TaskDay => 6;
+    private readonly ILogger<Day6Task> _logger;
+    #region TestData
+    protected override string TestData => @"";
+    #endregion
+
+    public Day6Task(IAdventWebClient client, ILogger<Day6Task> logger) : base(client)
+    {
+        _logger = logger;
+    }
+
+    public override async Task<string?> GetFirstTaskAnswerAsync()
+    {
+        _ = await GetDataAsListAsync<string>();
+        throw new TaskIncompleteException();
+    }
+
+    public override async Task<string?> GetSecondTaskAnswerAsync()
+    {
+        _ = await GetDataAsListAsync<string>();
+        throw new TaskIncompleteException();
+    }
+}
