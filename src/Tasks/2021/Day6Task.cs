5ccc76d9e95e7ef74a5414fd05028b06744e22ac
Richard Dyer <Richard@r7development.com>
2021-12-05 23:44:12 -0700

    Day 6 task

--- src/Tasks/2021/Day6Task.cs
+++ src/Tasks/2021/Day6Task.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace AdventCode.Tasks2021;
 
 public class Day6Task : BaseCodeTask, IAdventCodeTask
@@ -5,7 +7,8 @@ public class Day6Task : BaseCodeTask, IAdventCodeTask
     public override int TaskDay => 6;
     private readonly ILogger<Day6Task> _logger;
     #region TestData
-    protected override string TestData => @"";
+    //protected override string TestData => @"3,4,3,1,2";
+    protected override string TestData => @"3,4,3,1,2";
     #endregion
 
     public Day6Task(IAdventWebClient client, ILogger<Day6Task> logger) : base(client)
@@ -15,13 +18,55 @@ public class Day6Task : BaseCodeTask, IAdventCodeTask
 
     public override async Task<string?> GetFirstTaskAnswerAsync()
     {
-        _ = await GetDataAsListAsync<string>();
-        throw new TaskIncompleteException();
+        var testData = (await GetDataAsync()).Split(",").Select(x => int.Parse(x)).ToList();
+        //Straightforward solution
+        for (var i = 0; i < 80; i++)
+        {
+            var newFish = 0;
+            for (var fish = 0; fish < testData.Count; fish++)
+            {
+                if (testData[fish] == 0)
+                {
+                    testData[fish] = 6;
+                    newFish++;
+                }
+                else
+                {
+                    testData[fish]--;
+                }
+            }
+            for (int n = 0; n < newFish; n++)
+            {
+                testData.Add(8);
+            }
+        }
+        return testData.Count.ToString();
     }
-
     public override async Task<string?> GetSecondTaskAnswerAsync()
     {
-        _ = await GetDataAsListAsync<string>();
-        throw new TaskIncompleteException();
+        //straightforward solution doesn't work because array is too large. 
+        //Instead of tracking each fish, track how many fish are on each day so you now only have an array of size 8
+
+        var testData = (await GetDataAsync()).Split(",").Select(x => int.Parse(x)).GroupBy(x => x)
+                .ToDictionary(x => x.Key, x => (long)x.Count());
+
+        for (var i = 0; i < 256; i++)
+        {
+            //create a new Dictionary taking the previous days count and setting it to the next so 8-> 7-> 6. 
+            //However on Day 6 *and* Day 8 you are setting Day 0 since this is the exponential growth
+            testData = new Dictionary<int, long>()
+            {
+                [8] = testData.GetValueOrDefault(0),
+                [7] = testData.GetValueOrDefault(8),
+                [6] = testData.GetValueOrDefault(0) + testData.GetValueOrDefault(7),
+                [5] = testData.GetValueOrDefault(6),
+                [4] = testData.GetValueOrDefault(5),
+                [3] = testData.GetValueOrDefault(4),
+                [2] = testData.GetValueOrDefault(3),
+                [1] = testData.GetValueOrDefault(2),
+                [0] = testData.GetValueOrDefault(1),
+            };
+        }
+        return testData.Values.Sum().ToString();
     }
 }
