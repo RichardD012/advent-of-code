bd69b3b58f65bc6f5c6d173abea8056277362856
Richard Dyer <Richard@r7development.com>
2021-12-07 07:37:43 -0700

    Adding Day 8 boilerplate

---
+++ src/Tasks/2021/Day8Task.cs
@@ -0,0 +1,27 @@
+namespace AdventCode.Tasks2021;
+
+public class Day8Task : BaseCodeTask, IAdventCodeTask
+{
+    public override int TaskDay => 8;
+    private readonly ILogger<Day8Task> _logger;
+    #region TestData
+    protected override string TestData => @"";
+    #endregion
+
+    public Day8Task(IAdventWebClient client, ILogger<Day8Task> logger) : base(client)
+    {
+        _logger = logger;
+    }
+
+    public override async Task<string?> GetFirstTaskAnswerAsync()
+    {
+        _ = await GetDataAsListAsync<string>();
+        throw new TaskIncompleteException();
+    }
+
+    public override async Task<string?> GetSecondTaskAnswerAsync()
+    {
+        _ = await GetDataAsListAsync<string>();
+        throw new TaskIncompleteException();
+    }
+}
