fb2645d0cdc8f913f7a7163821b0e15d84ba96ac
Richard Dyer <Richard@r7development.com>
2021-12-06 22:16:37 -0700

    Day 7 task

--- src/Tasks/2021/Day7Task.cs
+++ src/Tasks/2021/Day7Task.cs
@@ -5,7 +5,7 @@ public class Day7Task : BaseCodeTask, IAdventCodeTask
     public override int TaskDay => 7;
     private readonly ILogger<Day7Task> _logger;
     #region TestData
-    protected override string TestData => @"";
+    protected override string TestData => @"16,1,2,0,4,2,7,1,2,14";
     #endregion
 
     public Day7Task(IAdventWebClient client, ILogger<Day7Task> logger) : base(client)
@@ -15,13 +15,54 @@ public class Day7Task : BaseCodeTask, IAdventCodeTask
 
     public override async Task<string?> GetFirstTaskAnswerAsync()
     {
-        _ = await GetDataAsListAsync<string>();
-        throw new TaskIncompleteException();
+        var testData = (await GetDataAsync()).Split(",").Select(x => int.Parse(x));
+        var min = testData.Min();
+        var max = testData.Max();
+        int? leastMoves = null;
+        for (var i = min; i <= max; i++)
+        {
+            int moves = 0;
+            foreach (var entry in testData)
+            {
+                moves += Math.Abs(entry - i);
+            }
+            if (leastMoves == null || moves < leastMoves)
+            {
+                leastMoves = moves;
+            }
+        }
+        return leastMoves.ToString();
     }
 
     public override async Task<string?> GetSecondTaskAnswerAsync()
     {
-        _ = await GetDataAsListAsync<string>();
-        throw new TaskIncompleteException();
+        var testData = (await GetDataAsync()).Split(",").Select(x => int.Parse(x));
+        var min = testData.Min();
+        var max = testData.Max();
+        int? leastMoves = null;
+        for (var i = min; i <= max; i++)
+        {
+            int moves = 0;
+            foreach (var entry in testData)
+            {
+                var steps = Math.Abs(entry - i);
+                moves += CalculateMoves(steps);
+            }
+            if (leastMoves == null || moves < leastMoves)
+            {
+                leastMoves = moves;
+            }
+        }
+        return leastMoves.ToString();
+    }
+    //There should probably be a better formula for this?
+    private static int CalculateMoves(int steps)
+    {
+        int returnInt = 0;
+        for (int i = 1; i <= steps; i++)
+        {
+            returnInt += i;
+        }
+        return returnInt;
     }
 }
